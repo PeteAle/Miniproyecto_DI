a0bc1f5d4114d27dc5b9bda22029a35aa4cdc106
PeteAle <yau17914@uvg.edu.gt>
2021-02-02 15:51:47 -0600

    Progra inicial del stepper y su driver sin el UART
    Se comenzó a programar el stepper tomando en cuenta el driver a utilizar pero sin incluir la comunicación serial.

---
+++ Stepper/Stepper.ino
@@ -0,0 +1,15 @@
+
+int pinPaso = ;
+int pinDir = ;
+int serialRecibido = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinPaso, OUTPUT);
+  pinMode(pinDir, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
