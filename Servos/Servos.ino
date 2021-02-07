a5ef1bec6c24f5b8255450c42fef7601968f54d4
PeteAle <yau17914@uvg.edu.gt>
2021-02-06 18:59:46 -0600

    Simulación de los motores en Proteus
    Se comenzó la simulación de los motores en Proteus y se hicieron arreglos al programa de los servos.

--- Servos/Servos.ino
+++ Servos/Servos.ino
@@ -2,37 +2,112 @@
 
 #include <Servo.h>
 
-int inData = 0;
-int boton_inc = 8;  // Botón para incrementar el valor del servo (movimiento derecho).
-int boton_dec = 7;  // Botón para decrementar el valor del servo (movimiento izquierdo).
-int press_inc = 0;  // Variable de estado del botón de incremento.
-int press_dec = 0;  // Variable de estado del botón de decremento.
-int giro = 90;
+const char longitud = 16;
+char inData = ' ';
+char index = 0;
+char recibido [longitud];
+char tempChars [longitud];
+int primerServo;
+int segundoServo;
+int tercerServo;
+int cuartoServo;
+#define corte ','
+#define inicio '<'
+#define fin '>'
+int giro = 0;
 
 Servo servo1;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  servoMotor.attach(9);
-  pinMode(boton_inc, INPUT);
-  pinMode(boton_dec, INPUT);
-  digitalWrite(boton_inc, HIGH);
-  digitalWrite(boton_dec, HIGH);
+  Serial.begin(115200);
+
+  Serial.println("Arduino conectado.");
+  
+  servo1.attach(9);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    // read the incoming byte:
+  recibirConMarcador();
+  strcpy(tempChars, recibido);
+  separarDatos();
+  showParsedData();
+  /*if (Serial.available() > 0) {
+    // read the incoming byte:   
     inData = Serial.read();
+    if (inData != corte){
+      recibido[index] = inData;
+      index = index + 1;
+      if (index > longitud){
+        index = longitud - 1;
+      }
+    }
+    else if (inData == corte){
+      recibido[index] = '\0';
+      index = 0;
+      primerServo = atoi(recibido);
+      segundoServo = atoi(recibido);
+    }
+    Serial.print("Primer servo: ");
+    Serial.println(primerServo);
+    Serial.print("Segundo servo: ");
+    Serial.println(segundoServo);
+    servo1.write(primerServo);
+    delay(15);
+  }*/
+}
+
+// ------------- Funciones adicionales -----------------------
+
+void recibirConMarcador(){
+  static boolean recibiendo = false;
+  while(Serial.available() > 0){
+    inData = Serial.read();
+    if (recibiendo == true){
+      if (inData != fin){
+      recibido[index] = inData;
+      index = index + 1;
+      }
+      if (index > longitud){
+        index = longitud - 1;
+      }
+    }
+    else{
+      recibido[index] = '\0';
+      index = 0;
+      recibiendo = false;
+    }
+    if (inData == inicio){
+      recibiendo = true;
+    }
   }
-  press_inc = digitalRead(boton_inc);
-  if (press_inc == LOW){
-    servo1.write(++giro);
-  }
-  press_dec = digitalRead(boton_dec)
-  if (press_dec == LOW){
-    servo1.write(--giro);
-  }
+}
+
+void separarDatos(){
+  char * token;
+  token = strtok(recibido, ',');
+  primerServo = atoi(token);
+
+  token = strtok(NULL, ',');
+  segundoServo = atoi(token);
+
+  token = strtok(NULL, ',');
+  tercerServo = atoi(token);
+
+  token = strtok(NULL, ',');
+  cuartoServo = atoi(token);
+}
+
+
+void showParsedData() {
+    Serial.print("Primer servo:  ");
+    Serial.println(primerServo);
+    Serial.print("Segundo servo: ");
+    Serial.println(segundoServo);
+    Serial.print("Tercer servo: ");
+    Serial.println(tercerServo);
+    Serial.print("Cuarto servo: ");
+    Serial.println(cuartoServo);
 }
--- Stepper/Stepper.ino
+++ Stepper/Stepper.ino
@@ -1,15 +1,18 @@
 
 int pinPaso = ;
 int pinDir = ;
-int serialRecibido = 0;
+int datoEntrante = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(pinPaso, OUTPUT);
   pinMode(pinDir, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  while (Serial.available() > 0){
+    datoEntrante = Serial.read();
+  }
 }
